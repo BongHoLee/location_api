f54b862c5dbc38055e4090c48ffd4d2932e07fd3
bongholee <leebongho9204@gmail.com>
2022-09-18 23:03:31 +0900

    dir(package): 패키지 구조 정의
    정의 내용:
    - application, domain, infrastructure 기본 구조 정의

---
+++ src/main/java/com/search/domain/repository/LocationRepository.java
@@ -0,0 +1,5 @@
+package com.search.domain.repository;
+
+public interface LocationRepository {
+
+}
--- src/test/java/com/search/infrastructure/repository/WebClientLocationRepositoryTest.java
+++ src/test/java/com/search/infrastructure/repository/WebClientLocationRepositoryTest.java
@@ -17,13 +17,35 @@ class WebClientLocationRepositoryTest {
     @Test
     @DisplayName("카카오 API 호출 테스트")
     void 카카오_API_호출_테스트() {
-        LocationGetQuery query1 = makeKakaoQueryFor("곱창");
+        /*
+        {
+	"lastBuildDate":"Sun, 18 Sep 2022 22:56:20 +0900",
+	"total":1,
+	"start":1,
+	"display":1,
+	"items":[
+		{
+			"title":"청어람 망원점",
+			"link":"",
+			"category":"한식>곱창,막창,양",
+			"description":"",
+			"telephone":"",
+			"address":"서울특별시 마포구 망원동 482-3",
+			"roadAddress":"서울특별시 마포구 망원로 97",
+			"mapx":"303671",
+			"mapy":"551215"
+		}
+	]
+}
+         */
+        LocationGetQuery query1 = makeKakaoQueryFor("청어람 망원점");
         LocationGetQuery query2 = makeKakaoQueryFor("갈비");
 
         Mono<String> searchResultMono1 = clientRequest(query1);
         Mono<String> searchResultMono2 = clientRequest(query2);
         List<Mono<?>> resultList = Arrays.asList(searchResultMono1, searchResultMono2);
         Object[] response = Mono.zip(resultList, result -> result).block();
+        System.out.println(response);
         assertThat(response).hasSize(resultList.size()).isNotNull();
     }
 
@@ -35,7 +57,8 @@ class WebClientLocationRepositoryTest {
         Mono<String> searchResultMono1 = clientRequest(query1);
         Mono<String> searchResultMono2 = clientRequest(query2);
         List<Mono<?>> resultList = Arrays.asList(searchResultMono1, searchResultMono2);
-
+        Object[] response = Mono.zip(resultList, result -> result).block();
+        System.out.println(response);
         assertThat(Mono.zip(resultList, result -> result).block())
                 .hasSize(resultList.size())
                 .isNotNull();
