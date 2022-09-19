ebb0c1346e911129324e420d07c491e0c0a35c74
leebong5 <leebongho5081@gmail.com>
2022-09-19 22:08:38 +0900

    ci(docker): docker-compose 수정 및 실행 환경 재구성

--- src/test/java/com/search/infrastructure/repository/WebClientLocationRepositoryTest.java
+++ src/test/java/com/search/infrastructure/repository/WebClientLocationRepositoryTest.java
@@ -17,27 +17,6 @@ class WebClientLocationRepositoryTest {
     @Test
     @DisplayName("카카오 API 호출 테스트")
     void 카카오_API_호출_테스트() {
-        /*
-        {
-	"lastBuildDate":"Sun, 18 Sep 2022 22:56:20 +0900",
-	"total":1,
-	"start":1,
-	"display":1,
-	"items":[
-		{
-			"title":"청어람 망원점",
-			"link":"",
-			"category":"한식>곱창,막창,양",
-			"description":"",
-			"telephone":"",
-			"address":"서울특별시 마포구 망원동 482-3",
-			"roadAddress":"서울특별시 마포구 망원로 97",
-			"mapx":"303671",
-			"mapy":"551215"
-		}
-	]
-}
-         */
         LocationGetQuery query1 = makeKakaoQueryFor("청어람 망원점");
         LocationGetQuery query2 = makeKakaoQueryFor("갈비");
 
