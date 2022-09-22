5b7960256f77c134bcc0079a2673b40a151bf0b8
leebong5 <leebongho5081@gmail.com>
2022-09-22 15:27:03 +0900

    feat(Cache): 장소 검색 결과 캐싱 기능 구현
    구현 내용:
    - 스프링 부트 캐시 서비스 추상화 활용
    - 캐싱 구현체 Redis 사용
    - 장소 검색 결과 캐싱 시간 60분 설정

---
+++ src/main/java/com/search/infrastructure/repository/location/LocationWebClients.java
@@ -0,0 +1,36 @@
+package com.search.infrastructure.repository.location;
+
+import static java.util.stream.Collectors.toList;
+
+import com.search.domain.vo.Search;
+import com.search.infrastructure.client.builder.ClientMonoBuilder;
+import com.search.infrastructure.entity.LocationEntities;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class LocationWebClients {
+    private final List<ClientMonoBuilder> clientMonoBuilders;
+
+    public LocationWebClients(List<ClientMonoBuilder> clientMonoBuilders) {
+        this.clientMonoBuilders = clientMonoBuilders;
+    }
+
+    // 결과 캐싱
+    @Cacheable(value = "LocationEntities", key = "#search")
+    public LocationEntities requestAbout(Search search) {
+
+        // WebClient 병렬 호출
+        LocationEntities locationEntities = new LocationEntities(new ArrayList<>());
+        clientMonoBuilders.stream()
+                .map(eachBuilder -> eachBuilder.buildFor(search))
+                .map(eachMono -> eachMono.share().block())
+                .collect(toList())
+                .forEach(locationEntities::add);
+
+        return locationEntities;
+    }
+}
