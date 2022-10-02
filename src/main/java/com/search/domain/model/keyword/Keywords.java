2576ed332a68a9704f35038cd9f69f16c78bcf93
leebongho <leebongho9204@gmail.com>
2022-10-02 18:49:28 +0900

    update

--- src/main/java/com/search/domain/model/keyword/Keywords.java
+++ src/main/java/com/search/domain/model/keyword/Keywords.java
@@ -1,11 +1,15 @@
 package com.search.domain.model.keyword;
 
+import com.search.domain.error.BusinessException;
+import com.search.domain.error.DomainErrorCode;
 import com.search.domain.model.keyword.function.KeywordsPostProcessor;
 import java.util.List;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
 
 @Getter
+@Slf4j
 public class Keywords {
     private final List<Keyword> keywords;
 
@@ -15,5 +19,12 @@ public class Keywords {
 
     public Keywords postProcessBy(KeywordsPostProcessor postProcessor) {
         return postProcessor.postProcess(this);
+    }
+
+    private void validation(List<Keyword> keywords) {
+        if (keywords == null || keywords.isEmpty()) {
+            log.error("keywords cannot be empty");
+            throw new BusinessException(DomainErrorCode.INVALID_KEYWORDS_ERROR);
+        }
     }
 }
